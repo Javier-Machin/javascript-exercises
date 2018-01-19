ab588f3dde7530662932d1e95d35c33976288a89
Javier Machin <javier_machin_acosta@hotmail.com>
2018-01-19 17:41:02 +0000

    Add calculator solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,53 @@
-function add () {
-	
+function add (value1, value2) {
+	return value1 + value2;
 }
 
-function subtract () {
-	
+function subtract (value1, value2) {
+	return value1 - value2;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((sum, value) => sum += value,0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((total, value) => total *= value,1);
 }
 
-function power() {
-	
-}
+function power(value1, value2) {
+	let result = 1;
+	for (let i = 0; i < value2;i++) {
+		result *=  value1;
+	}
+	return result;
+} //  return Math.pow(value1, value2)
 
-function factorial() {
+function factorial(value) {
+	if (value == 0) return 1;
+	let factorialNums = [value];
+
+	for (let i = value; i > 1; i--) {
+		factorialNums.push(i - 1);
+	}
+
+	//let factorialResult = 1;
 	
-}
+	//for (let i = 0; i < factorialNums.length; i++) {
+	//	factorialResult *= factorialNums[i];
+	//}
+
+	return factorialNums.reduce((total,value) => total *= value);
+
+	// alternative version
+
+  //if (n == 0) return 0;
+  //let product = 1;
+  //for (let i = n; i > 0; i--) {
+  //  product *= i;
+  //}
+  //return product;
+  	
+
 
 module.exports = {
 	add,
