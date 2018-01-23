37db339d8dcab15f0c502f2078a0226c6c6aa90a
Javier Machin <javier_machin_acosta@hotmail.com>
2018-01-23 11:28:14 +0000

    Add fibonacci solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-var fibonacci = function() {
+var fibonacci = function(number) {
+	if (Number(number) < 0 || !(Number(number)) ) { return "OOPS" };
+	number = Number(number);
 
+	let result = [0,1];
+	for (let i = 1; i < number; i++) {
+		result.push(result[i-1] + result[i]);
+	}	
+	return Number(result.slice(-1));
 }
+
+//solution without array
+//if (count < 0) return "OOPS";
+//  let a = 0;
+//  let b = 1;
+//  for (let i = 1; i < count; i++) {
+//    const temp = b;
+//    b = a + b;
+//    a = temp;
+//  }
+//  return b;
+//};
+
 
 module.exports = fibonacci
